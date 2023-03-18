96b2f87cdda95595c76d777351e90ec264b3457a
Andre Igor <andre.igor@pd.griaule>
2023-03-18 11:03:02 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Volumetric Image Visualization - 1s2023 - André Nóbrega
---
+++ task1/getslices.c
@@ -0,0 +1,15 @@
+/*
+Computing Institute - Unicamp
+Volumetric Image Visualization - MO815 - 1s2023
+Professor: Alexandre Falcão
+Student:   André Nóbrega
+*/
+
+#include "ift.h"
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+}
